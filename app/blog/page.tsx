999bc4e1ef596d5f3faecdac5432eba6b60c0986
yeowon083 <yeowon083@gmail.com>
2026-04-20 11:45:23 +0900

    fix blog title

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -330,7 +330,7 @@ export default async function BlogPage({
         ) : (
           <>
             <h1 className="text-4xl md:text-5xl font-bold tracking-tight leading-tight mb-6">
-              글 목록
+              블로그 글 목록
             </h1>
 
             <p className="text-lg text-gray-600 leading-8">
